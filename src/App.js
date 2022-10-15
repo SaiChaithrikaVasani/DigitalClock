21162b64178d0c6c85166da0fe844731076a4335
SaiChaithrikaVasani <96117347+SaiChaithrikaVasani@users.noreply.github.com>
2022-10-15 10:40:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import DigitalClock from "../Components/DigitalClock";
+
+export default function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <DigitalClock />
+    </div>
+  );
+}
